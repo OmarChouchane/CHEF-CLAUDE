4d040ad6cea9ac73dab42cab9599097891899e25
OmarChouchane <omar.ch52831@gmail.com>
2025-02-23 20:11:29 +0100

    header

---
+++ CHEF-CLAUDE/src/Header.jsx
@@ -0,0 +1,10 @@
+import chefClaudeLogo from '../src/assets/images/chef-claude-icon.png';
+
+export default function Header() {
+    return (
+        <header>
+            <img src={chefClaudeLogo} alt="chef claude logo" srcset="" />
+            <h1>Chef Claude's Recipes</h1>
+        </header>
+    );
+}
