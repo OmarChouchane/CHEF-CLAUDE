e8d12857590e4ffa3876e6f5f41a24379227faf9
OmarChouchane <omar.ch52831@gmail.com>
2025-02-24 16:18:16 +0100

    conditional rendering - get a recipe

--- CHEF-CLAUDE/src/Main.jsx
+++ CHEF-CLAUDE/src/Main.jsx
@@ -3,23 +3,25 @@ import React from "react"
 export default function Main() {
 
     const [ingredients, setIngredients] = React.useState([])
-        
+
     const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
     ))
 
-    function handleSubmit(event) {
-
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
+    function addIngredient(formData) {
         const newIngredient = formData.get("ingredient")
-        setIngredients( prevIngredients => [...prevIngredients, newIngredient])
-
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
+    
+    /**
+     * Challenge:
+     * Using conditional rendering, only render the new <section> IF
+     * there are ingredients added to the list of ingredients.
+     */
 
     return (
         <main>
-            <form onSubmit={handleSubmit} className="add-ingredient-form">
+            <form action={addIngredient} className="add-ingredient-form">
                 <input
                     type="text"
                     placeholder="e.g. oregano"
@@ -28,9 +30,17 @@ export default function Main() {
                 />
                 <button>Add ingredient</button>
             </form>
-            <ul>
-                {ingredientsListItems}
-            </ul>
+            { ingredients.length > 0 && <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+            </section>}
         </main>
     )
 }
