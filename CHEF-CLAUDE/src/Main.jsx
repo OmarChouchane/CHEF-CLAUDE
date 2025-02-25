f23129c0cbca3ef6d1173708016e42e61ba68042
OmarChouchane <omar.ch52831@gmail.com>
2025-02-25 17:24:38 +0100

    refactor to separate components

--- CHEF-CLAUDE/src/Main.jsx
+++ CHEF-CLAUDE/src/Main.jsx
@@ -1,24 +1,24 @@
 import React from "react"
+import ClaudeRecipe from "../src/components/ClaudeRecipe"
+import IngredientsList from "../src/components/IngredientsList"
 
 export default function Main() {
 
-    const [ingredients, setIngredients] = React.useState([])
+    const [ingredients, setIngredients] = React.useState(
+        ["all the main spices", "pasta", "ground beef", "tomato paste"]
+    )
 
-    const ingredientsListItems = ingredients.map(ingredient => (
-        <li key={ingredient}>{ingredient}</li>
-    ))
+    const [recipeShown, setRecipeShown] = React.useState(false)
+
 
     function addIngredient(formData) {
         const newIngredient = formData.get("ingredient")
         setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
-    
-    /**
-     * Challenge:
-     * Only display the div.get-recipe-container if the ingredients list
-     * has more than 3 items in it. (Fewer than that and it might not
-     * give great results from the chef 🤖👩‍🍳)
-     */
+
+    function toggleRecipeShown() {
+        setRecipeShown(prevShown => !prevShown)
+    }
 
     return (
         <main>
@@ -31,17 +31,12 @@ export default function Main() {
                 />
                 <button>Add ingredient</button>
             </form>
-            {ingredients.length > 0 && <section>
-                <h2>Ingredients on hand:</h2>
-                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
-                {ingredients.length > 3 && <div className="get-recipe-container">
-                    <div>
-                        <h3>Ready for a recipe?</h3>
-                        <p>Generate a recipe from your list of ingredients.</p>
-                    </div>
-                    <button>Get a recipe</button>
-                </div>}
-            </section>}
+            {ingredients.length > 0 && 
+                <IngredientsList 
+                    ingredients={ingredients} 
+                    toggleRecipeShown={toggleRecipeShown}
+                />}
+            {recipeShown && <ClaudeRecipe />}
         </main>
     )
 }
