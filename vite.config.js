d07b6828b6f9d2e01e705a267e778c817482dbfe
OmarChouchane <omar.ch52831@gmail.com>
2025-03-02 04:42:12 +0100

    updatedto vercel

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+
+import dotenv from "dotenv";
+dotenv.config();
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'process.env': process.env
+  },
+  build: {
+    outDir: 'dist'  // ✅ Ensure Vercel can find the build output
+  }
+})
