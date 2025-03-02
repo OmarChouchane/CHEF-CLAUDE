4474052e3aba343c31db23eeb91ccd30c5f94584
OmarChouchane <omar.ch52831@gmail.com>
2025-03-02 15:23:19 +0100

    ready for vercel

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,9 @@ export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': process.env
-  }
+  },
+  root: '.',  // Ensure root is set to the project folder
+  build: {
+    outDir: 'dist',  // Output folder (default is "dist")
+  },
 })
